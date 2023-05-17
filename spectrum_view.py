88d451c18b93deff4efd1d0cbaac99d00941c845
Cyber-Shuo <117975967+Cyber-Shuo@users.noreply.github.com>
2023-05-17 14:16:39 +0800

    Add files via upload
    V1.0

---
+++ spectrum_view.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.collections import LineCollection
+from PIL import Image
+import numpy as np
+from pathlib import Path
+import re
+import string
+from scipy.stats import norm
+from scipy.optimize import curve_fit
+from astropy.modeling import models, fitting
+
+def GaussFunc(x, counts, mu, sigma):
+    return counts*np.exp(-((x-mu)/np.sqrt(2)/sigma)**2)
+
+def getdata(path,a,A,total_A,mean_A):
+    with open(path,'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        value = [float(s) for s in line.split()]
+        a.append(value[0])
+        A.append(value[1])
+
+    MAX_A = a[A.index(max(A))]
+    print('MAX =', MAX_A)
+
+    for i in range(0, len(A)):
+        total_A = total_A + A[i]
+
+    for j in range(0, len(A)):
+        mean_A = mean_A + A[j]
+        if mean_A <= total_A/2:
+            continue
+        else:
+            mean_A = a[j]
+            print('mean_A =', mean_A)
+            break
+
+def gauss_fit(a, A, fit_A_list):
+    popt, pcov = curve_fit(GaussFunc, a, A, bounds =([100,400,0],[100000,500,100]))
+    print(popt)
+    for i in a:
+        fit_A = GaussFunc(i, *popt)
+        fit_A_list.append(fit_A)
+    return fit_A_list
+
+fig, ax = plt.subplots()
+
+path_a = Path("D:\STUDY_think\Data\\fiber_Collimators_data\\domestic-led-427nm-filter.txt")
+a, A, total_A, mean_A, fit_A_list  = [],[],0,0,[]
+getdata(path_a, a, A, total_A, mean_A)
+gauss_fit(a, A, fit_A_list)
+ax.plot(a, fit_A_list, color = 'r', label = 'domestic-led-427nm-filter-gaussfit')
+ax.plot(a, A, '.', markersize = 0.3, color = 'black', label = 'domestic-led-427nm-filter')
+
+path_b = Path("D:\STUDY_think\Data\\fiber_Collimators_data\\domestic-led.txt")
+b, B, total_B, mean_B, fit_B_list  = [],[],0,0,[]
+getdata(path_b, b, B, total_B, mean_B)
+gauss_fit(b, B, fit_B_list)
+ax.plot(b, fit_B_list, color = 'g', label = 'domestic-led-gaussfit')
+ax.plot(b, B, 'x', markersize = 0.3, color = 'black', label = 'domestic-led')
+
+path_c = Path("D:\STUDY_think\Data\\fiber_Collimators_data\\austrian-led-427nm-filter.txt")
+c, C, total_C, mean_C, fit_C_list  = [],[],0,0,[]
+getdata(path_c, c, C, total_C, mean_C)
+gauss_fit(c, C, fit_C_list)
+ax.plot(c, fit_C_list, color = 'b', label = 'austrian-led-427nm-filter-gaussfit')
+ax.plot(c, C, '--', markersize = 0.3, color = 'black', label = 'austrian-led-427nm-filter')
+
+path_d = Path("D:\STUDY_think\Data\\fiber_Collimators_data\\austrian-led.txt")
+d, D, total_D, mean_D, fit_D_list  = [],[],0,0,[]
+getdata(path_d, d, D, total_D, mean_D)
+gauss_fit(d, D, fit_D_list)
+ax.plot(d, fit_D_list, color = 'y', label = 'austrian-led-gaussfit')
+ax.plot(d, D, 'o', markersize = 0.3, color = 'black', label = 'austrian-led')
+
+
+plt.xlabel('wavelength/nm')
+plt.ylabel('counts')
+plt.legend()
+plt.show()
+
